f5687d0efb804a8a830de3846319694306c802c8
yeongbin-gong <youngbin816@gmail.com>
2022-01-21 15:21:09 +0900

    prettier setting

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import express from 'express';
 const app: express.Application = express();
 
 app.get('/', (res: express.Response) => {
-  res.send("hello express !");
+  res.send('hello express !');
 });
 
 app.listen(3000, () => {
   console.log('실행중');
-})
+});
