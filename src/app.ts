eea027e1685459d2af8e92be3c280bf5e4ff1b3c
yeongbin-gong <youngbin816@gmail.com>
2022-01-24 14:18:21 +0900

    add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config({ path: __dirname + `/../.env.${process.env.NODE_ENV}` });
+
 const app: express.Application = express();
 
-app.get('/', (res: express.Response) => {
+app.set('port', 3000 || process.env.PORT);
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  console.log(process.env.TEST);
   res.send('hello express !');
 });
 
-app.listen(3000, () => {
-  console.log('실행중');
+app.listen(app.get('port'), () => {
+  console.log(`${app.get('port')}번 포트에서 서버 실행 중...`);
 });
