3a622e852f178689552a022957afe46cc54e582f
yeongbin-gong <youngbin816@gmail.com>
2022-02-18 11:53:10 +0900

    express validation tracking add

--- src/api/routes/tracking.ts
+++ src/api/routes/tracking.ts
@@ -1,11 +1,36 @@
 import { NextFunction, Router, Request, Response } from 'express';
+import { oneOf, query, Result, validationResult } from 'express-validator';
 
 const route: Router = Router();
+
+const validateReq = [
+  // param('token').isUUID(4),
+  query('clickId').notEmpty(),
+  query('pubId').notEmpty(),
+  query('subId').optional(),
+  oneOf([query('idfa').exists({ checkFalsy: true }), query('adid').exists({ checkFalsy: true })]),
+  query('sub1').optional(),
+  query('sub2').optional(),
+  query('sub3').optional(),
+  query('sub4').optional(),
+  query('sub5').optional(),
+];
+
+const result = (req: Request, res: Response, next: NextFunction) => {
+  const errors: Result = validationResult(req);
+  if (!errors.isEmpty()) res.status(400).send(errors);
+  next();
+};
 
 export default (app: Router) => {
   app.use('/', route);
 
-  route.get('/:token', async function (req: Request, res: Response, next: NextFunction) {
-    return res.status(200).json('노에러');
-  });
+  route.get(
+    '/:token',
+    validateReq,
+    result,
+    async function (req: Request, res: Response, next: NextFunction) {
+      return res.status(200).json('');
+    }
+  );
 };
